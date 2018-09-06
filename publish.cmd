15c0599f90d59b7465451c36d04c83500cf9c20f
QuickHealer <coryhealey@hotmail.com>
2018-09-06 11:15:14 -0700

    Adding source code sync'd from Azure

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=quickhealerchatter-Web-Deploy.pubxml -p:Password=LFHaaE6CzYfBgoNw6CNykxNojMridngEf1iiGfTlsamod4ovMfbvENypGT7d
+
